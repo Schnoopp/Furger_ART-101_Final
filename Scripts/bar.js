4551b816e13279e469f30a1204d6f8331a46738b
Schnoopp <quokadda@gmail.com>
2026-05-25 15:47:39 -0700

    sneding object home functionality. added extra ingredients

--- Scripts/bar.js
+++ Scripts/bar.js
@@ -2,40 +2,47 @@ let isHoldingObject = false;
 let heldobject;
 let ingredientList = [];
 
-const allIngredients = document.getElementsByClassName("item");
-console.log(allIngredients)
+
 
 getAllIngredients()
 
 
-
+// collects all objects of class item, creates an array of ingredients: name, position values
 function getAllIngredients() {
+  const allIngredients = document.getElementsByClassName("item");
+  console.log(allIngredients)
   for (let i = 0; i < allIngredients.length; i++) {
     const ingredientName = allIngredients[i].id
 
     position = allIngredients[i].getBoundingClientRect();
     const top = position.top;
     const left = position.left;
-    
+
 
     const ingredient = {
       name: ingredientName,
-      startingPosition_L: top,
-      startingPosition_T: left
+      startingPosition_L: left,
+      startingPosition_T: top
 
     }
-
-    ingredientList.push(ingredient)
+    ingredientList.push(ingredient);
   }
   console.log(ingredientList)
   return
 
+};
 
-}
+//compares variable to all ingredients, checks if it's in array and returns object value
+function getItemValue(object) {
+  for (let i = 0; i < ingredientList.length; i++) {
 
+    if (object == ingredientList[i].name) {
+      console.log("item found, value is: " + i)
+      return i
+    };
 
-
-
+  }
+};
 
 
 $('.item').click(function () {
@@ -47,6 +54,9 @@ $('.item').click(function () {
   }
   else if (clickedObject == heldobject) {
     console.log("this is the same thing " + heldobject)
+    itemLetGo()
+
+
   }
 });
 
@@ -64,9 +74,26 @@ $(document).mousemove(function (event) {
 
 $(document).keydown(function (event) {
   if (event.key === " " || event.code === "Space") {
-    isHoldingObject = false;
-    heldobject = null;
+    itemLetGo()
   }
 });
 
 
+function itemLetGo() {
+  let itemIndex = getItemValue(heldobject)
+  console.log(itemIndex)
+  isHoldingObject = false;
+
+
+
+  console.log("LEFT: "+ingredientList[itemIndex].startingPosition_L)
+  console.log("TOP: "+ingredientList[itemIndex].startingPosition_T)
+
+  let startingLeft = ingredientList[itemIndex].startingPosition_L
+  let startingTop = ingredientList[itemIndex].startingPosition_T
+
+  $("#" + heldobject).css("left", startingLeft + "px")
+  $("#" + heldobject).css("top", startingTop + "px")
+  heldobject = null;
+
+}

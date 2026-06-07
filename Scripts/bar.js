321473caafc0535ce396af18441ec046ff88f326
Sasha :3 <quokadda@gmail.com>
2026-06-06 21:22:25 -0700

    guh

--- Scripts/bar.js
+++ Scripts/bar.js
@@ -1,30 +1,56 @@
 let isHoldingObject = false;
 let heldobject;
 let ingredientList = [];
-let hoveredObject = null;
-let recipieInProgress
+let activeRecipie;
+
+let drinkInProgress = [];
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 const recipies = [];
-recipies.push({ name: "Screwdriver", ingredients: ["vodka", "oj"] })
-recipies.push({ name: "Dirty Shirley", ingredients: ["vodka", "grenadine", "cherries"] })
-recipies.push({ name: "Marg", ingredients: ["tequilla", "lj", "lime", "salt"] })
+recipies.push({ name: "Screwdriver", ingredients: ["vodka", "orangeJuice"] })
+recipies.push({ name: "Dirty Shirley", ingredients: ["vodka", "grenadine", "cherries", "limeJuice"] })
+recipies.push({ name: "Lemon Drop", ingredients: ["vodka", "tripleSec", "lemonJuice", "syrup", "sugar"] })
+recipies.push({ name: "White Russian", ingredients: ["vodka", "kahula", "creamLiqueur", "cherries"] })
+
+recipies.push({ name: "Margarita", ingredients: ["tequilla", "limeJuice", "lime", "salt", "sugar"] })
+recipies.push({ name: "Tequila Sunrise", ingredients: ["tequilla", "orangeJuice", "grenadine", "cherries"] })
+recipies.push({ name: "Tequila Espresso Martini", ingredients: ["tequilla", "kahula", "syrup"] })
+recipies.push({ name: "Matador", ingredients: ["tequilla", "pineappleJuice", "limeJuice", "tripleSec"] })
+
+recipies.push({ name: "Gin and Tonic", ingredients: ["gin", "tonic", "lime"] })
+recipies.push({ name: "Gimlet", ingredients: ["gin", "syrup", "limeJuice", "lime"] })
+recipies.push({ name: "Tom Collins", ingredients: ["gin", "lemonJuice", "syrup", "tonic", "cherries"] })
+recipies.push({ name: "Gin Sour", ingredients: ["gin", "lemonJuice", "syrup", "bitters"] })
+
+recipies.push({ name: "Mojito", ingredients: ["rum", "limeJuice", "mint", "sugar", "tonic"] })
+recipies.push({ name: "Bahama Mama", ingredients: ["rum", "kahula", "pineappleJuice", "cherries"] })
+recipies.push({ name: "Daquiri", ingredients: ["rum", "limeJuice", "sugar", "syrup"] })
+recipies.push({ name: "Mai Tai", ingredients: ["rum", "tripleSec", "pineappleJuice", "orangeJuice", "syrup", "grenadine"] })
+
+recipies.push({ name: "Old Fashioned", ingredients: ["whiskey", "syrup", "bitters", "cherries"] })
+recipies.push({ name: "Revolver", ingredients: ["whiskey", "kahula", "bitters"] })
+recipies.push({ name: "B52 Shot", ingredients: ["whiskey", "kahula", "creamLiqueur", "triplesec"] })
+recipies.push({ name: "Whiskey Sour", ingredients: ["whiskey", "lemonJuice", "syrup", "cherries"] })
 
 console.log(recipies)
 selectRecipie()
 
+
 function selectRecipie() {
-  let activeRecipie = Math.floor(Math.random() * (recipies.length));
-  recipieInProgress = recipies[activeRecipie]
-  console.log(recipieInProgress)
-  console.log(recipieInProgress.ingredients)
-  displayRecipie()
+  activeRecipie = recipies[Math.floor(Math.random() * (recipies.length))]
+  console.log(activeRecipie)
+  console.log(activeRecipie.ingredients)
+  displayRecipie(activeRecipie)
 };
 
-function displayRecipie() {
-
-  for (let i = 0; i < recipieInProgress.ingredients.length; i++) {
-    $(".menu").append("<div>" + recipieInProgress.ingredients[i] + "</div>")
-    console.log("appended: " + recipieInProgress.ingredients[i])
+function displayRecipie(recipie) {
+  $("#menuText").append("<div>" + recipie.name + ": </div>" + "<p>ingredients: </p>")
+  for (let i = 0; i < recipie.ingredients.length; i++) {
+    $("#menuText").append("<div>" + recipie.ingredients[i] + "</div>")
+    console.log("appended: " + recipie.ingredients[i])
   }
 }
 
@@ -37,7 +63,6 @@ function getAllIngredients() {
   const allIngredients = document.getElementsByClassName("item");
 
   for (let i = 0; i < allIngredients.length; i++) {
-    const ingredientName = allIngredients[i].id
 
     position = allIngredients[i].getBoundingClientRect();
     const top = position.top;
@@ -45,7 +70,7 @@ function getAllIngredients() {
 
 
     const ingredient = {
-      name: ingredientName,
+      name: allIngredients[i].id,
       startingPosition_L: left,
       startingPosition_T: top
     }
@@ -100,6 +125,37 @@ $(".shelf_container").click(function () {
 
 });
 
+let label_show = false;
+
+$(".shelf_container").hover(
+  function () {
+    if (isHoldingObject == false) {
+      if (label_show == false) {
+        $(".label").stop(true, true).slideDown(300);
+        $(".label").html(ingredientList[this.id].name)
+        console.log(ingredientList[this.id].name)
+        label_show = true
+
+      }
+    }
+
+
+
+  },
+  function () {
+
+    label_show = false
+    delay(1000).then(() => {
+      if (label_show == false) {
+        $(".label").stop(true, true).slideUp(300);
+      }
+
+    })
+
+
+  }
+);
+
 
 
 function itemLetGo() {
@@ -118,23 +174,19 @@ function itemLetGo() {
 
 
 
-
-
-
-
-
 function checkIngredient(ing) {
-  console.log(recipieInProgress.ingredients.length)
+  console.log(activeRecipie.ingredients.length)
 
-  for (let i = 0; i < recipieInProgress.ingredients.length; i++) {
+  for (let i = 0; i < activeRecipie.ingredients.length; i++) {
 
-    if (ing == recipieInProgress.ingredients[i]) {
+    if (ing == activeRecipie.ingredients[i]) {
       console.log("HUZZAH")
-      recipieInProgress.ingredients.splice(i)
+      drinkInProgress.push(heldobject);
+      console.log(drinkInProgress)
+
       return
     }
   }
-
 }
 
 
@@ -147,7 +199,6 @@ $(".shaker").click(function () {
   //add ingredient to cup and mark as added ingredient
   //put ingredient on back of shelf
 
-
   if (isHoldingObject == true) {
     checkIngredient(heldobject)
     console.log("added ingredient: " + heldobject)
@@ -157,8 +208,44 @@ $(".shaker").click(function () {
 
 });
 
+$("#done").click(function () {
+  if (compareArrays(drinkInProgress, activeRecipie.ingredients) == true) {
+    console.log("ALLDONE")
+  }
+  else(
+    console.log("dumbass your drink not done")
+  )
 
 
+});
+
+
+function compareArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  console.log("array euqal length")
+
+  const sortedArr1 = arr1;
+  const sortedArr2 = arr2;
+
+  sortedArr1.sort(function (a, b) {
+    var textA = a.toUpperCase();
+    var textB = b.toUpperCase();
+    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+  });
+
+  sortedArr2.sort(function (a, b) {
+    var textA = a.toUpperCase();
+    var textB = b.toUpperCase();
+    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+  });
+
+  for (let i = 0; i < sortedArr1.length; i++) {
+    if (sortedArr1[i] !== sortedArr2[i]) return false;
+  }
+  return true;
+
+
+};
 
 
 // document bs
